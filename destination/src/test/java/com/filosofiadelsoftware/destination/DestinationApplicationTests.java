40ee7f0ab22e6894f1b9317fd66ff138ca7d0d3c
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-03-22 20:03:57 -0500

    Initial commit

---
+++ destination/src/test/java/com/filosofiadelsoftware/destination/DestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filosofiadelsoftware.destination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
